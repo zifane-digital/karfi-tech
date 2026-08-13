ad1ea2b2c050bcc6ec6deb9227ba643d0a7b6289
souleymamann-source <souleymamann@gmail.com>
2026-08-13 01:12:43 +0100

    feat: initial commit

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 import { prisma } from "@/lib/prisma";
 // GET : récupérer les utilisateurs
